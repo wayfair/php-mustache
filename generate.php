b50444c0e880e1f977d107f373076505e267165c
John Boehr <jbboehr@gmail.com>
2013-05-27 23:09:57 -0700

    Added google analyics to web

--- generate.php
+++ generate.php
@@ -29,6 +29,15 @@ ob_start();
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-7764958-4', 'jbboehr.github.io');
+      ga('send', 'pageview');
+    </script>
   </head>
   <body>
     
